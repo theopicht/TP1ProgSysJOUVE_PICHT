798db6a46ee5ae2dc539d7b412266ccd51de8cb4
Theo Picht <theo.picht@gmail.com>
2023-11-29 14:04:31 +0100

    TP1 Q1 : Creation of enseash.

---
+++ enseash/main.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+int main() {
+    //Instead of using a printf, we're gonna use the read-write logic
+    const char welcome_message[] = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n";
+    write(STDOUT_FILENO, welcome_message, sizeof(welcome_message) - 1);
+
+    const char prompt[] = "enseash % ";
+    write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
+
+    while (1) {
+        //We're gonna place all the REPL logic there
+    }
+
+    return 0;
+}
