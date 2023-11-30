bd76e5682f663bdfca4fb8fcb3cb5ebb84a2b2a4
Theo Picht <theo.picht@gmail.com>
2023-12-01 00:00:22 +0100

    TP1 Q4 : Display status + Code formating + Documentation

--- enseash/main.c
+++ enseash/main.c
@@ -1,8 +1,12 @@
-#include <unistd.h>
+// Includes
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
+// Constants
 #define EXCLUDE_NULL_CHAR 1 // Define EXCLUDE_NULL_CHAR to exclude the null char at the end of the prompt.
 #define MAX_COMMAND_LENGTH 100 // Define the maximal size of a command
 #define LINE_BREAK_DELETE_OFFSET 1 // Define LINE_BREAK_DELETE_OFFSET to remove the line break
@@ -15,17 +19,32 @@ void displayWelcomeMessage() {
 }
 
 void executeCommand(const char *command) {
+    // Create a child process
     pid_t pid = fork();
+    int status;
+    // Prompt buffer
+    char statusBuffer[512];
 
-    if (pid == 0) { // Child process
+    // If the process is a child process
+    if (pid == 0) {
         execlp(command, command, NULL);
-        // If execlp returns, there is an error
-        write(STDOUT_FILENO, "Error executing command\n", strlen("Error executing command\n"));
+        // If execlp returns, there was an error
+        perror("Error while executing command");
         exit(EXIT_FAILURE);
-    } else if (pid > 0) { // Parent process
-        waitpid(pid, NULL, 0);
+    } else if (pid > 0) { // If the process is a parent process
+        // Wait for the child process to finish and get the exit status
+        waitpid(pid, &status, 0);
+
+        // Print the prompt with the exit status or the signal information
+        if (WIFEXITED(status)){ // If the process exited normally
+            sprintf(statusBuffer, "enseah [exit:%d] %% ", WEXITSTATUS(status));
+            write(STDIN_FILENO, statusBuffer, strlen(statusBuffer));
+        } else if(WIFSIGNALED(status)){ // If the process was killed by a signal
+            sprintf(statusBuffer, "enseah [exit:%d] %% ", WTERMSIG(status));
+            write(STDIN_FILENO, statusBuffer, strlen(statusBuffer));
+        }
     } else { // Fork failed
-        write(STDOUT_FILENO, "Error creating child process\n", strlen("Error creating child process\n"));
+        perror("Error while creating child process");
         exit(EXIT_FAILURE);
     }
 }
@@ -40,23 +59,30 @@ void displayFortune() {
     executeCommand("fortune");
 }
 
+// The main function is the entry point of the program. It is called when the program is executed.
 int main() {
+    // Instead of using a printf, we are going to use the read-write logic
 
-    // Instead of using a printf, we're gonna use the read-write logic
+    // Display the welcome message
+    displayWelcomeMessage();
+
+    // Define the prompt and the command
     const char prompt[] = "enseash % ";
     char command[MAX_COMMAND_LENGTH];
 
-    displayWelcomeMessage();
-
+    // Display the prompt
+    write(STDOUT_FILENO, prompt, sizeof(prompt) - EXCLUDE_NULL_CHAR);
     while (1) {
-        write(STDOUT_FILENO, prompt, sizeof(prompt) - EXCLUDE_NULL_CHAR);
-        // We are gonna write all the REPL logic there
+        // We are going to write all the REPL logic there
 
         // Reading of the typed command
         ssize_t command_length = read(STDIN_FILENO, command, sizeof(command));
 
         // Check for end of file (Ctrl+D to exit)
         if (command_length == 0) {
+            // If the command is "exit", display a bye bye message and exit the program
+            const char exit_message[] = "Bye bye...";
+            write(STDOUT_FILENO, exit_message, sizeof(exit_message) - EXCLUDE_NULL_CHAR);
             break;
         }
 
@@ -68,11 +94,14 @@ int main() {
 
         // Exit if the command is "exit"
         if (strcmp(command, "exit") == 0) {
+            // If the command is "exit", display a bye bye message and exit the program
+            const char exit_message[] = "Bye bye...";
+            write(STDOUT_FILENO, exit_message, sizeof(exit_message) - EXCLUDE_NULL_CHAR);
             break;
         }
 
         // If nothing typed, display date
-        if (command_length == 0) {
+        if (command_length == 0 || strcmp(command, "date") == 0) {
             displayDate();
         } else if (strcmp(command, "fortune") == 0) {
             // If the command is "fortune", display a fortune
